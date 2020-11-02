20632201c7f0817cf0095e4246ffcedc18b82be9
Arniel <aceba1@outlook.com>
2020-11-01 23:58:49 -0500

    Clear unnecessary content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
 
 function App() {
-  const [state, setState] = useState({})
   return (
-    <div className="App" onClick={()=>{setState({backgroundColor:'#243964'})}}>
-      <header className="App-header" style={state}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Running on <code>{window.navigator.userAgent}</code>
-        </p>
-        <label className="App-link">
-          Consume Legume
-        </label>
-      </header>
     </div>
   );
 }
