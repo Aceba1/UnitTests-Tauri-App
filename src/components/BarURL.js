46fe8faaa327846348aa068933d0cbd789ec6e5e
Arniel <aceba1@outlook.com>
2020-11-05 01:19:14 -0500

    Create BarURL.js

---
+++ src/components/BarURL.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { RequestContext } from '../contexts/RequestContext'
+import Button from './Button';
+import Input from './Input';
+import Options from './Options';
+
+/*
+  GET     - Default
+  POST    - No length restrictions
+  PUT     - PUT but responses shouldn't change
+  HEAD    - GET but no response body
+  DELETE  - 
+  PATCH   - 
+  OPTIONS - 
+*/
+
+export default function BarURL() {
+  const { url, setUrl, type, setType, doRequest } = useContext(RequestContext);
+  return (
+    <div className="BarURL">
+      <Options items={["GET", "POST", "PUT", "HEAD", "DELETE", "PATCH", "OPTIONS"]} value={type} setValue={setType} />
+      <Input className="URL" placeholder="Target URL" value={url} setValue={setUrl} />
+      <Button className={"Send " + type} text={type} onClick={doRequest} />
+    </div>
+  )
+}
