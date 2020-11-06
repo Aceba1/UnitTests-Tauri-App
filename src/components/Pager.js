f55c9f8a0231bac06f04c88a1894880ed23f3ed3
Arniel <aceba1@outlook.com>
2020-11-06 02:00:02 -0500

    Populate Pager.js

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -3,4 +3,35 @@
 // Calls store and load events to pages, by RequestContext and tabber index changes
 // - Must use some kind of hook; A new context?
 
-  //TODO: Populate!
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import Tabber from './Tabber'
+  
+  function Pager(props) {
+    const [page, setPage] = useState(0)
+    const Page = props.pages[page];
+    return (
+      <div className={"Pager " + props.className} >
+        <Tabber 
+          index={page} 
+          setIndex={setPage} 
+          items={props.pages} 
+          className={props.className} />
+        <div className={"Page " + props.className} >
+          <Page
+            //TODO: Figure out what exactly to pass to Page
+          />
+        </div>
+      </div>
+    )
+  }
+  
+  Pager.propTypes = {
+    className: PropTypes.string,
+    pages: PropTypes.array,
+
+  }
+  
+  export default Pager
+  
+  
