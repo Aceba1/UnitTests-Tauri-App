5e7ef788e128c9302baacdce118fc06560fd2c71
Arniel <aceba1@outlook.com>
2020-11-06 01:56:18 -0500

    Populate Tab.js and Tabber.js

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -3,4 +3,30 @@
 // on click:
 // - calls back to tabber current index
 
-  //TODO: Populate!
+  import React from 'react'
+  import PropTypes from 'prop-types'
+import Button from './Button'
+  
+  function Tab(props) {
+    return (
+      <Button 
+        key={props.index} 
+        className={props.className + " Tab " + props.active} 
+        text={props.text} 
+        onClick={() => props.setActive(props.index)} />
+    )
+  }
+  
+  Tab.propTypes = {
+    index: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired,
+    setActive: PropTypes.func.isRequired,
+
+    className: PropTypes.string,
+
+  }
+  
+  export default Tab
+  
+  
--- src/components/Tabber.js
+++ src/components/Tabber.js
@@ -1,1 +1,34 @@
-// holds Tabs  //TODO: Populate!
+// holds Tabs
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import Tab from './Tab'
+
+function Tabber(props) {
+  return (
+    <div className={"Tabber " + props.className}>
+      { props.items.map((item, index) => {
+        return (
+          <Tab 
+            text={item.name} 
+            index={index} 
+            key={index} 
+            active={index===props.index} 
+            setActive={props.setIndex} 
+            className={props.className} />
+        )
+      })}
+    </div>
+  )
+}
+
+Tabber.propTypes = {
+  items: PropTypes.array.isRequired,
+  index: PropTypes.number.isRequired,
+  setIndex: PropTypes.func.isRequired,
+
+  className: PropTypes.string,
+}
+
+export default Tabber
+
