9b9b24289ff0b05b24769432dd4f28fc4b0bcf1b
Arniel <aceba1@outlook.com>
2020-11-11 23:55:40 -0500

    Improve Results visualizer

--- src/components/Response.js
+++ src/components/Response.js
@@ -1,14 +1,35 @@
 import React, { useContext } from 'react'
 import { RequestContext } from '../contexts/RequestContext'
+import TextContextProvider from '../contexts/TextContext';
+//import Body from './PageBody';
+import Pager from './Pager';
+import TextPropsJSON from './TextPagePropsJSON';
+import TextRaw from './TextPageRaw';
 
 export default function Response() {
-  const { resp } = useContext(RequestContext);
-  //TODO: Populate!
-  return (
-    <div>
-      <code>{resp}</code>
-    </div>
+  const { resp, status } = useContext(RequestContext);
+
+  return resp != null ? (
+    <TextContextProvider 
+      text={resp}
+      setText={() => {}}
+    >
+      <Pager 
+        className="Response BodyPager code"
+        pages={[TextRaw, TextPropsJSON] 
+        /*
+          TODO: Conditionally show TextPropsJSON if data is valid JSON
+          Possibly consider HTML page?
+        */} 
+      />
+    </TextContextProvider>
+  ) : (
+    <div 
+      className="Response linebreak EmptyResponse" 
+      dangerouslySetInnerHTML={{__html: status}} 
+    />
   )
+  //TODO: Resolve header acquisition
   /*
     <ContentContextProvider 
       header={resp.header}
